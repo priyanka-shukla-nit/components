d891b0b4ad28d0c8eb07a6e8ac4a893307adab88
priyanka-shukla-nit <113821977+priyanka-shukla-nit@users.noreply.github.com>
2022-09-26 01:33:42 +0530

    Update AddTOCart.js

--- AddTOCart.js
+++ AddTOCart.js
@@ -1,1 +1,21 @@
+import React, { useState } from 'react'
+import PlusMinusButton from '../componenet/PlusMinusButton'
 
+const AddTOCart = () => {
+   const [showquantityButton,setshowquantityButton]  = useState(false)
+
+   const handelquantity = (e) =>{
+    setshowquantityButton(true)
+   }
+
+
+   {
+    if(showquantityButton === true){
+      return <PlusMinusButton/>
+    } else{
+      return <button className='order-class' onClick={handelquantity}>order</button>
+    }
+  }
+}
+
+export default AddTOCart
